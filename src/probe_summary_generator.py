8a9d2e4038dca0d7b7c155404977fec59c86ca3e
Ozkan Aydemir <ozkan_aydemir@brown.edu>
2019-12-03 12:12:27 -0500

    update to vcf_to_tables to supply a separate min_qual value for targeted sites. update to probe_summary_generator to use call_info dictionary instead of mip_info dictionary for when the latter is not available. Updates to the test data analysis notebooks.

--- src/probe_summary_generator.py
+++ src/probe_summary_generator.py
@@ -62,3 +62,34 @@ def get_probe_info(probe_set_keys=None, output_file=None,
     if output_file is not None:
         info_df.to_csv(output_file, index=False)
     return info_df
+
+
+def get_probe_call_info(probe_set_keys=None, output_file=None,
+                        probe_sets_file=None, call_info_file=None):
+    used_probes = set()
+    if probe_sets_file is None:
+        probe_sets_file = "/opt/project_resources/mip_ids/probe_sets.json"
+    if call_info_file is None:
+        call_info_file = "/opt/project_resources/mip_ids/call_info.json"
+    if probe_set_keys is not None:
+        for psk in probe_set_keys:
+            with open(probe_sets_file) as infile:
+                used_probes.update(json.load(infile)[psk])
+
+    with open(call_info_file) as infile:
+        mip_info = json.load(infile)
+    info_list = []
+    for g in mip_info:
+        for m in mip_info[g]:
+            if (probe_set_keys is None) or (m in used_probes):
+                minfo = mip_info[g][m]["copies"]
+                for c in minfo:
+                    copy_dict = minfo[c]
+                    copy_dict["Gene"] = g
+                    copy_dict["MIP"] = m
+                    copy_dict["Copy"] = c
+                    info_list.append(pd.DataFrame(copy_dict, index=[0]))
+    info_df = pd.concat(info_list, ignore_index=True)
+    if output_file is not None:
+        info_df.to_csv(output_file, index=False)
+    return info_df
