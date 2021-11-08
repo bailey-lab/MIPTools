888a3829d7183f00d61e4b10153dab3d07227b40
Aris Paschalidis <aris.paschalidis@gmail.com>
2021-11-08 12:19:41 -0500

    Use Github Pages to deploy docs

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = "Bailey Lab"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ["sphinx.ext.githubpages"]  # add .nojekyll to gh-pages
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
