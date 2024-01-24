1b9c975fc4c839b1ddb2f5c9d97d46246ab53339
Alfred Simkin <alfred.simkin@gmail.com>
2024-01-24 08:38:47 -0500

    new mip_function concatenate_headers

--- src/mip_functions.py
+++ src/mip_functions.py
@@ -5330,6 +5330,28 @@ def vcf_reheader(vcf_file, fixed_vcf_file, wdir="/opt/analysis/"):
                     vcf_path,  "-o", fixed_vcf_path], check=True)
     return
 
+def concatenate_headers(settings=None, wdir='/opt/analysis', freebayes_settings=None):
+	import yaml
+	contig_vcf_gz_paths_yaml = open("/opt/analysis/contig_vcf_gz_paths.yaml",'r')
+	contig_vcf_gz_paths = yaml.safe_load(contig_vcf_gz_paths_yaml)
+	vcf_file="/opt/analysis/variants.vcf.gz"
+	# concatanate contig vcfs. The number of contigs may be high, so we'll
+	# write the vcf paths to a file and bcftools will read from that file
+	cvcf_paths_file = os.path.join(wdir, "contig_vcfs", "vcf_file_list.txt")
+	with open(cvcf_paths_file, "w") as outfile:
+	    outfile.write("\n".join(contig_vcf_gz_paths) + "\n")
+	subprocess.run(["bcftools", "concat", "-f", cvcf_paths_file, "-Oz",
+                "-o", vcf_file], check=True)
+	subprocess.run(["bcftools", "index", "-f", vcf_file], check=True)
+	# fix vcf header if --gvcf option has been used
+	if "--gvcf" in freebayes_settings:
+	    temp_vcf_path = os.path.join(wdir, "temp.vcf.gz")
+	    vcf_reheader(os.path.basename(vcf_file), temp_vcf_path, wdir=wdir)
+	    old_vcf_path = os.path.join(wdir, "unfixed.vcf.gz")
+	    subprocess.run(["mv", vcf_file, old_vcf_path])
+	    subprocess.run(["mv", temp_vcf_path, vcf_file])
+	    subprocess.run(["bcftools", "index", "-f", vcf_file], check=True)
+	    print('did a reheader')
 
 def gatk(options):
     """GATK wrapper function.
