b953ad719f07da1d298667ae35210deb8965ec42
alfredsimkin <alfred.simkin@gmail.com>
2024-06-25 18:07:23 -0400

    added jeff instructions

--- user_scripts/start_jupyter.sh
+++ user_scripts/start_jupyter.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-
-project_resources='/home/charlie/resources/miptools_test-data/DR1_project_resources'
-species_resources='/home/charlie/resources/miptools_test-data/pf_species_resources'
-wrangler_folder='/home/charlie/resources/miptools_test-data_wrangler'
-variant_output='/home/charlie/projects/MIPTools/user_scripts/local_files/variant_jupyter'
-sif_file='/home/charlie/resources/mip.sif'
-prevalence_metadata='/home/charlie/resources/choropleth_inputs'
+sif_file=miptools_dev.sif
+project_resources=input_data/DR23K_project_resources
+species_resources=input_data/pf_species_resources
+prevalence_metadata=input_data/metadata_files
+wrangler_folder=wrangled_data
+variant_output=stats_and_variant_calling
 
 singularity run \
   -B $project_resources:/opt/project_resources \
