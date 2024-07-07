7e4cbdc79a0ed855de1f862732a9333a5bd980db
Charlie Simkin <charlie.simkin@gmail.com>
2024-07-07 12:05:06 -0600

    update locations of files

--- src/calculate_prevalences.py
+++ src/calculate_prevalences.py
@@ -101,37 +101,3 @@ def calculate_prevalences(metadata_file, prevalences_input_table, mutations, out
 	count_dict, mutation_dict = get_counts(prevalences_input_table, site_dict)
 	create_output_file(count_dict, mutations, output_summary_table)
 	# print(count_dict)
-
-def load_json(region_of_interest):
-	with urlopen('https://raw.githubusercontent.com/simkin-bioinformatics/geojson_files/main/'+region_of_interest+'.geojson') as response:
-		region_df = json.load(response)
-	return region_df
-
-def read_prevalence_table(sample_set, variants_of_interest):
-	summary_table = sample_set+'_prevalence_summary.tsv'
-	prevalence_summary_df = pd.read_csv(summary_table, sep='\t')
-	for variant in variants_of_interest:
-		prevalence_summary_df[variant]=[float(x.split()[0]) for x in prevalence_summary_df[variant]]
-	return prevalence_summary_df
-
-def display_choropleth(region_of_interest, variant, sample_set, variants_of_interest):
-	region_dict = {'uganda':{'zoom':5.8, 'coordinates':{"lat": 1.3733, "lon": 32.2903}},
-                   'tanzania':{'zoom':4, 'coordinates':{"lat": -6.3690, "lon": 34.8888}}
-                  }
-	json_file = load_json(region_of_interest)
-	prevalence_summary_df = read_prevalence_table(sample_set, variants_of_interest)
-	fig = px.choropleth_mapbox(prevalence_summary_df, 
-                                geojson=json_file, 
-                                locations='Sites', 
-                                color=variant,
-                                color_continuous_scale="reds",
-                                mapbox_style="open-street-map",
-                                featureidkey="properties.name",
-                                zoom=region_dict[region_of_interest]['zoom'], 
-                                center = region_dict[region_of_interest]['coordinates'],
-                                labels={'Sites':'Site'},
-                                range_color=(0,1),
-    )
-	fig.update_layout(margin={"r":0,"t":40,"l":0,"b":0})
-	fig.update_layout(height=500, title=sample_set)
-	return fig
