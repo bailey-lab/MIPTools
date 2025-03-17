878755b4a8127e4f2645ddc905ae2563b48fbffd
Charlie Simkin <charlie.simkin@gmail.com>
2025-03-17 13:32:30 -0400

    add comments about how to use MIPTools.def and build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,6 @@
-# be sure to update the version in MIPTools.def before running this script
+# this script reads the version of the miptools file in MIPTools.def and 
+# creates a sif file with the same name.  It is a handy way to ensure the naming convention
+# remains constant for newly created sif files
 
 eval $(grep -h "MIPTOOLS_VERSION=" MIPTools.def)
 
